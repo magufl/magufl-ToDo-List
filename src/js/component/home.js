75a50d129fa50543465883c0d467426785bde2de
Naiceiry <83250687+Naiceiry@users.noreply.github.com>
2021-06-12 00:09:57 +0200

    Update home.js

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,10 +1,21 @@
 import React from "react";
-import { Navbar } from "./navbar";
+import { Navbar } from "./navbar.js";
+import { Jumbotron } from "./jumbotron.js";
+import { Card } from "./card.js";
+import { Footer } from "./futer.js";
 //create your first component
 export function Home() {
+	const tittle = ["Hola", "oi", "Hello"];
 	return (
-		<div className="text-center mt-5">
+		<div className="text-center mt-2">
 			<Navbar />
+			<Jumbotron />
+			<div className="row justify-content-center">
+				{tittle.map((item, index) => {
+					return <Card key={index} text={item} />;
+				})}
+			</div>
+			<Footer />
 		</div>
 	);
 }
