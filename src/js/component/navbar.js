a524a3f6af4d0a1f8a536e128eccb33ecbd39754
Naiceiry <83250687+Naiceiry@users.noreply.github.com>
2021-06-14 00:13:49 +0200

    Add files via upload

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,7 +3,7 @@ export function Navbar() {
 	return (
 		<nav className="navbar navbar-expand-lg navbar-light bg-light navbar navbar-dark bg-dark">
 			<a className="navbar-brand" href="#">
-				Navbar
+				Start Bootstrap
 			</a>
 			<button
 				className="navbar-toggler"
@@ -16,10 +16,8 @@ export function Navbar() {
 				<span className="navbar-toggler-icon"></span>
 			</button>
 
-			<div
-				className="collapse navbar-collapse"
-				id="navbarSupportedContent">
-				<ul className="navbar-nav mr-auto">
+			<div className="collapse navbar-collapse text-right" id="left">
+				<ul className="navbar-nav mr-auto ">
 					<li className="nav-item active">
 						<a className="nav-link" href="#">
 							Home <span className="sr-only">(current)</span>
@@ -27,34 +25,13 @@ export function Navbar() {
 					</li>
 					<li className="nav-item">
 						<a className="nav-link" href="#">
-							Link
+							About
 						</a>
 					</li>
 					<li className="nav-item dropdown">
-						<a
-							className="nav-link dropdown-toggle"
-							href="#"
-							id="navbarDropdown"
-							role="button"
-							data-toggle="dropdown"
-							aria-haspopup="true"
-							aria-expanded="false">
-							Dropdown
+						<a className="nav-link" href="#">
+							Servise
 						</a>
-						<div
-							className="dropdown-menu"
-							aria-labelledby="navbarDropdown">
-							<a className="dropdown-item" href="#">
-								Action
-							</a>
-							<a className="dropdown-item" href="#">
-								Another action
-							</a>
-							<div className="dropdown-divider"></div>
-							<a className="dropdown-item" href="#">
-								Something else here
-							</a>
-						</div>
 					</li>
 					<li className="nav-item">
 						<a
@@ -62,23 +39,10 @@ export function Navbar() {
 							href="#"
 							tabIndex="-1"
 							aria-disabled="true">
-							Disabled
+							contact
 						</a>
 					</li>
 				</ul>
-				<form className="form-inline my-2 my-lg-0">
-					<input
-						className="form-control mr-sm-2"
-						type="search"
-						placeholder="Search"
-						aria-label="Search"
-					/>
-					<button
-						className="btn btn-outline-success my-2 my-sm-0"
-						type="submit">
-						Search
-					</button>
-				</form>
 			</div>
 		</nav>
 	);
