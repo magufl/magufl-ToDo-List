309681b7b6515abf3ad8c53e17082dc4b200ece8
NayraBH <nayrabhernandez@gmail.com>
2022-01-13 20:21:33 +0000

    fix: footer style

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
 	return (
-		<div className="container-fluid bg-dark p-3 m-3">
+		<div className="container-fluid bg-dark p-3">
 			<p className="text-white text-center">
 				Copyright © Your Website 2022
 			</p>
