72cd5ac3d22c538347ae3ea063d5179996fbfcf4
faldea1 <faldea1@uc.cl>
2022-12-14 19:19:15 +0000

    all components

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Cards from "./Cards.jsx";
 import Footer from "./Footer.jsx";
+import Header from "./header.jsx";
+import Jumbotron from "./jumbotron.jsx";
 
 //create your first component
 const Home = () => {
   return (
     <div>
+      <Header />
+      <Jumbotron />  
       <Cards />
       <Footer />
     </div>
