9fbc5d86791a9a9da5f6c787e8e594d87b0fd84c
José Inácio Castelo Branco <zeinaciocb@gmail.com>
2023-01-30 19:03:48 +0000

    feat: jumbotron file added

---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+//create your first component
+const Jumbotron = props => {
+	return (
+		<div className="text-center">
+			<h1 className="text-center mt-5">Hello Rigo!</h1>
+			<p>
+				<img src={rigoImage} />
+			</p>
+			<a href="#" className="btn btn-success">
+				If you see this green button... bootstrap is working...
+			</a>
+			<p>
+				Made by{" "}
+				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+				love!
+			</p>
+		</div>
+	);
+};
+
+export default Jumbotron;
