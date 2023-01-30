f99a9137084f7caba571e8a5871c661357ba4a83
José Inácio Castelo Branco <zeinaciocb@gmail.com>
2023-01-30 23:21:52 +0000

    feat: jumbotron && added to home page component

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -3,21 +3,20 @@ import React from "react";
 //create your first component
 const Jumbotron = props => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+        <div className="p-5 mb-4 bg-light rounded-3">
+            <div className="container-fluid py-5">
+                <h1 className="display-5 fw-bold">{props.header}</h1>
+                <p className="fs-4">{props.description}</p>
+                <button className="btn btn-primary btn-lg" type="button">{props.buttonLabel}</button>
+            </div>
+        </div>
 	);
 };
+
+/*Jumbotron.propTypes = {
+    header: PropTypes.string,
+    description: PropTypes.string,
+    buttonLabel: PropTypes.string
+}*/
 
 export default Jumbotron;
