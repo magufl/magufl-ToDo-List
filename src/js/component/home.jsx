cc235398f1de4b27f1e0f0bec1b3ed47ca7689a4
sabonrex <sabona.a.rexwinkel@gmail.com>
2023-01-31 14:21:16 +0000

    ttest
    update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,16 +1,18 @@
+// notice that we have to import react for every component!
 import React from "react";
 
 //include your components into your bundle
-import Jumbotron from "./jumbotron.jsx";
+//here we will import all our components to make the home page component <Home />
+import CardComponent from "./card.jsx";
 
 //create your first component
 const Home = () => {
 	return (
-	<Jumbotron 
-		header="A Warm Welcome!"
-		description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-		buttonLabel="Let's Go!" 
-		/>
+        // a component always has to have only one parent node (tag) with every child and sibling nodes inside.
+        // hence this <div />, because we will add the other components inside Div
+		<div>
+			<CardComponent />
+		</div>
 	);
 };
 
