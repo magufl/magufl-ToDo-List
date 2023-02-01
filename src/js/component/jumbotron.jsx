37c51a71e4a00516757e5f06e7393d9e7820c000
José Inácio Castelo Branco <zeinaciocb@gmail.com>
2023-02-01 18:43:32 +0000

    fix: visual changes to get everything consistent

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 //create your first component
 const Jumbotron = props => {
 	return (
-        <div className="p-5 mb-4 bg-light rounded-3">
-            <div className="container-fluid py-5">
+        <div className="p-1 p-sm-5 m-0 m-sm-4 bg-light rounded-3">
+            <div className="container py-5">
                 <h1 className="display-5 fw-bold">{props.header}</h1>
                 <p className="fs-4">{props.description}</p>
                 <button className="btn btn-primary btn-lg" type="button">{props.buttonLabel}</button>
