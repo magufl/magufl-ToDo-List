57fd82a6c9237a0fc4c26a4252e265c7d6b9dbd9
romanconstantin1 <romanconstantin1@gmail.com>
2023-02-01 13:10:22 +0000

    Added proptypes checking

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const  NavbarComponent = (props) => {
   return (
@@ -21,6 +22,9 @@ const  NavbarComponent = (props) => {
   )
 };
 
-
+NavbarComponent.propTypes = {
+  badge: PropTypes.string,
+  buttons: PropTypes.array
+};
 
 export default NavbarComponent;
