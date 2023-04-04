2c2fedbef3f711ac41e7187e0252bc5089fd7d2b
levanmamuladze <levanmamoulatze@gmail.com>
2023-04-04 08:53:49 +0000

    card done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,14 +8,28 @@ import  Card  from "./card.jsx";
 const Home = () => {
 	const cards = [1, 2, 3, 4]; 
 	const cardComponents = []; 
-  
+    
 	for (let i = 0; i < cards.length; i++) {
 	  cardComponents.push(<Card key={cards[i]} />);
+
 	}
   
 	return (
-	  <div className=" d-flex justify-content-around">
-		{cardComponents}
+		<div className="container-fluid">
+		<div className="row justify-content-around">
+		  <div className="col-lg-3 col-md-6 mb-4">
+			{cardComponents[0]}
+		  </div>
+		  <div className="col-lg-3 col-md-6 mb-4">
+			{cardComponents[1]}
+		  </div>
+		  <div className="col-lg-3 col-md-6 mb-4">
+			{cardComponents[2]}
+		  </div>
+		  <div className="col-lg-3 col-md-6 mb-4">
+			{cardComponents[3]}
+		  </div>
+		</div>
 	  </div>
 	);
   };
