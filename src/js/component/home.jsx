e2e28f61189686f8a37582d78548dd5b4a4f311f
CL4UD3PT <cifpinho@gmail.com>
2023-04-12 19:17:45 +0000

    update: home.jsx code updated with first Todo List project home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,77 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
+	const [newTodo, setNewTodo] = useState("");
+	const [todoList, setTodoList] = useState([]);
+	const [visible, setVisible] = useState([]);
+  
+	const normalizeTodo = (e) => {
+	  let newTodo = e.target.value.toLowerCase();
+	  if (
+		e.key == "Enter" &&
+		newTodo !== ""
+	  ) {
+		setTodoList([...todoList, newTodo.charAt(0).toUpperCase() + newTodo.slice(1)]);
+		setNewTodo("");
+	  }
+	};
+  
+	const handleMouseEnter = (index) => {
+	  setVisible((prevState) => {
+		console.log(prevState)
+		const newState = [...prevState];
+		newState[index] = true;
+		return newState;
+	  });
+	};
+  
+	const handleMouseLeave = (index) => {
+	  setVisible((prevState) => {
+		const newState = [...prevState];
+		newState[index] = false;
+		return newState;
+	  });
+	};
+  
+	const removeTodo = (index) => {
+	  setTodoList(() => {
+		const newList = [...todoList];
+		newList.splice(index, 1);
+		return newList;
+	  });
+	};
+  
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	  <div className="container text-center col-6 vh-100">
+		<h1>Todo List</h1>
+		<input
+		  className="w-100 form-control"
+		  value={newTodo}
+		  placeholder={todoList.length <= 0 ? "No tasks, add a task!" : "Add a new todo"}
+		  onChange={(e) => setNewTodo(e.target.value)}
+		  onKeyUp={(e) => normalizeTodo(e)}
+		/>
+		<ul className="list-group mt-3 shadow">
+		  {todoList.map((todo, index) => {
+			return (
+			  <li
+				className="list-group-item list-group-item-action d-flex justify-content-between align-items-center fs-5"
+				key={index}
+				onMouseEnter={() => handleMouseEnter(index)}
+				onMouseLeave={() => handleMouseLeave(index)}
+			  >
+				{todo}
+				<i
+				  className={`remove-btn bi bi-x fs-3 text-danger ${visible[index] ? "" : "invisible"}`}
+				  onClick={() => removeTodo(index)}
+				></i>
+			  </li>
+			);
+		  })}
+		</ul>
+	  </div>
 	);
-};
-
-export default Home;
+  };
+  
+  export default Home;
