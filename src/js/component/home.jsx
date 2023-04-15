308443596becf8f01f275d87d3f9e9256dca1d30
CL4UD3PT <cifpinho@gmail.com>
2023-04-15 08:54:43 +0000

    features: remove task added; onChange input checks value with regexp;  fix: normalize task function working as expected;

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
 	const [todoList, setTodoList] = useState([{}]);
 	const [visible, setVisible] = useState([]);
 
+	// API constants and variables
 	const urlToAPI = "https://assets.breatheco.de/apis/fake/todos/user/CL4UD3PT";
 	const optionsGetTodos = {method: "GET",	headers: {'Content-Type' : 'application/json'}}
 
@@ -21,41 +22,56 @@ const Home = () => {
 		setTodoList(data);
 	}
 
-	// add new task to API
-	const updateTodos = async (e, newTask) => {
-		if(e.key == "Enter"){
-			setNewTodo("");
-			let tasks = [...todoList, { label : newTask, done : false }];
+	// Handle new todo
+	const handleNewTodoList = (e) => {
+		// remove white spaces around todo
+		let todo = e.target.value;
+		console.log('before normalize', todo);
+		
+		// todo must have at least one letter or a number
+		let regex = /^(?=.*[a-zA-Z0-9]).+$/;
+		if (regex.test(todo)) {
+			todo = normalizeTodo(todo);
 
-			const response = await fetch(urlToAPI, {
-				method: "PUT",
-				headers: { "Content-Type": "application/json"},
-				body: JSON.stringify(tasks)
-			})
-			// .then(resp => {
-			// 	console.log(resp.ok); // will be true if the response is successfull
-			// 	console.log(resp.status); // the status code = 200 or code = 400 etc.
-			// 	console.log(resp.text()); // will try return the exact result as string
-			// 	return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
-			// })
-			// .then(data => {
-			// 	//here is where your code should start after the fetch finishes
-			// 	console.log(data); //this will print on the console the exact object received from the server
-			// })
-			// .catch(error => {
-			// 	//error handling
-			// 	console.log(error);
-			// });
+			
+			
+			// todo must be unique to add to todoList and update API
+			if (todoList.filter(t => t.label === todo).length === 0 && e.key === 'Enter'){
+				const newTodoList = [...todoList, { label : todo, done : false }];
+				updateTodos(newTodoList);
+			}
+		}else{console.log(`The task "${todo}" it's not valid!`)};
+	}
+
+	const normalizeTodo = (todo) => {
+		// removing white spaces around and lowercase it
+		let task = todo.trim().toLowerCase();
+		
+		// normalize condition depending on length of task
+		// capitalizes first letter
+		task.length > 1
+		? task = task.charAt(0).toUpperCase() + task.slice(1)
+		: task = task.charAt(0).toUpperCase();
+		
+		return task;
+	};
+
+	// update tasks at backend API
+	// not optimized for large lists cause it's sending and retrieving the entire list
+	const updateTodos = async (list = todoList) => {	
+		console.table(list);
+		const response = await fetch(urlToAPI, {
+			method: "PUT",
+			headers: { "Content-Type": "application/json"},
+			body: JSON.stringify(list)
+		})
+		
+		// if response is ok then local list its updated and input element is reseted
+		if(response.ok) {
+			setTodoList(list);
+			setNewTodo("");
 		}
 	}
-	
-	const normalizeTodo = (task) => {
-		if (task !== "") {
-	  		let newTodo = task.toLowerCase().trim();;
-			newTodo = newTodo.charAt(0).toUpperCase() + newTodo.slice(1);
-			return newTodo;
-	  	}
-	};
   
 	const handleTaskMouseEnter = (index) => {
 	  setVisible((prevState) => {
@@ -73,14 +89,6 @@ const Home = () => {
 	  });
 	};
   
-	const removeTodo = (index) => {
-	  setTodoList(() => {
-		const newList = [...todoList];
-		newList.splice(index, 1);
-		return newList;
-	  });
-	};
-  
 	return (
 	  <div className="container text-center col-6 vh-100">
 		<h1>Todo List</h1>
@@ -89,24 +97,26 @@ const Home = () => {
 		  value={newTodo}
 		  placeholder={todoList.length <= 0 ? "No tasks, add a task!" : "Add a new todo"}
 		  onChange={(e) => setNewTodo(e.target.value)}
-		  onKeyUp={(e) => updateTodos(e, normalizeTodo(e.target.value))}
+		  onKeyUp={(e) => handleNewTodoList(e)}
 		/>
 		<ul className="list-group mt-3 shadow">
 		  {todoList.map((todo, index) => {
-			return (
-			  <li
-				className="list-group-item list-group-item-action d-flex justify-content-between align-items-center fs-5"
-				key={index}
-				onMouseEnter={() => handleTaskMouseEnter(index)}
-				onMouseLeave={() => handleTaskMouseLeave(index)}
-			  >
-				{todo.label}
-				<i
-				  className={`remove-btn fa-regular fa-circle-xmark fs-3 text-danger ${visible[index] ? "" : "invisible"}`}
-				  onClick={() => removeTodo(index)}
-				></i>
-			  </li>
-			);
+			if(!todo.hidden){
+				return (
+				<li
+					className="list-group-item list-group-item-action d-flex justify-content-between align-items-center fs-5"
+					key={index}
+					onMouseEnter={() => handleTaskMouseEnter(index)}
+					onMouseLeave={() => handleTaskMouseLeave(index)}
+				>
+					{todo.label}
+					<i
+					className={`remove-btn fa-regular fa-circle-xmark fs-3 text-danger ${visible[index] ? "" : "invisible"}`}
+					onClick={() => updateTodos(todoList.filter((task, i) => i != index))}
+					></i>
+				</li>
+				);
+			}
 		  })}
 		</ul>
 	  </div>
