d61a2def23210b9c85b02c140da40d1a314819f4
Daniel Carrión Martínez <danicm89@gmail.com>
2023-10-16 13:55:16 +0000

    Componente Texto

---
+++ src/js/component/texto.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Texto = () => {
+	return (
+		<div className="text-center">
+
+            <p>
+                hola
+            </p>
+			
+		</div>
+	);
+};
+
+export default Texto;
