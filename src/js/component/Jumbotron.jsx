f59d8bf7ff35beac0d0c4cbee110991b8b8f768f
Adrian <142854436+withyso@users.noreply.github.com>
2023-10-29 04:01:30 +0000

    Cambios esteticos finales, nuevo header para el Jumbotron

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -5,7 +5,7 @@ export const Jumbotron = () => {
     return (
         
         <div className="jumbotron">
-            <h1 className="display-4">A Warm welcome!</h1>
+            <h1 className="display-2 fw-bolder">A Warm Welcome!</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit temporibus ut deserunt sequi minus, quod rem esse, at, aspernatur tempore assumenda ullam possimus iste nam sint dicta voluptates odio ex!</p>
             <a className="btn btn-dark btn-lg" href="#" role="button">Call to action!</a>
         </div>
