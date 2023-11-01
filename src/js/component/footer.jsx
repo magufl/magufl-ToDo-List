7d01133b59bdaab994039840337f10391db9727d
Miguel5040 <miguelssj5040@gmail.com>
2023-11-01 07:03:19 +0000

    Agregar componentes footer y cards

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "../../styles/index.css";
+
+const Footer = () => {
+    const copyright = "Copyright &#169;";
+    return (
+        <footer className="footer container-fluid">
+            <div>Copyright &copy; Your website 2023</div>
+        </footer>
+    )
+}
+
+export default Footer;
