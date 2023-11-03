f55357c49b8c9d60b3da941a87bc7c2ba8582372
Miguel5040 <miguelssj5040@gmail.com>
2023-11-03 20:31:43 +0000

    Correccion de errores

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Jumbotron = () => {
   return (
-    <div class="jumbotron container">
+    <div className="jumbotron container">
       <h1 className="display-4 titulo">A warm Welcome!</h1>
       <p className="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa maxime nisi quis velit reiciendis ducimus fugit totam, autem omnis. Eius eos tenetur laborum corrupti maiores architecto, est modi eligendi qui!.</p>
 
