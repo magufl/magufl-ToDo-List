18cf92d1f96c532a1c6412a8c423f7230f24f73c
LisarWisar <60940343+LisarWisar@users.noreply.github.com>
2023-12-14 19:55:41 +0000

    Navbar completed

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -4,27 +4,19 @@ function Navbar() {
     let content= (
         <nav className="navbar navbar-expand-md navbar-dark bg-dark px-4">
             <a className="navbar-brand" id="navbar-brand" href="#">Start Bootstrap</a>
-            <div className="dropdown">
-                <button className="navbar-toggler" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li><a class="dropdown-item" href="#">About</a></li>
-                    <li><a class="dropdown-item" href="#">Services</a></li>
-                    <li><a class="dropdown-item" href="#">Portfolio</a></li>
-                    <li><a class="dropdown-item" href="#">Contact</a></li>
-                </ul>
-            </div>
-            <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div className=" collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
+                    <li className="nav-item px-2">
+                        <a className="nav-link active" href="#">Home</a>
+                    </li>
                     <li className="nav-item px-2">
                         <a className="nav-link" href="#">About</a>
                     </li>
                     <li className="nav-item px-2">
                         <a className="nav-link" href="#">Services</a>
-                    </li>
-                    <li className="nav-item px-2">
-                        <a className="nav-link" href="#">Portfolio</a>
                     </li>
                     <li className="nav-item px-2">
                         <a className="nav-link" href="#">Contact</a>
