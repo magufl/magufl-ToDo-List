c7ef77f292f0f6b5f1e10ab66f2769105d7af942
LisarWisar <60940343+LisarWisar@users.noreply.github.com>
2023-12-15 00:36:02 +0000

    Added jumbotron, cards, and footer

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,11 +2,21 @@ import React from "react";
 
 //include images into your bundle
 import Navbar from "./navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
+import Cards from "./card.jsx"
+import Footer from "./footer.jsx"
 
 //create your first component
 const Home = () => {
 	return (
-		<Navbar />
+		<div>
+			<Navbar />
+			<div className="container">
+				<Jumbotron />
+				<Cards />
+			</div>
+			<Footer />
+		</div>
 	);
 };
 
